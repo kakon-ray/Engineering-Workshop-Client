5923610d0c38bcff89a647c838ce4782741d6ae8
Kakon Ray <thisiskakonray@gmail.com>
2022-05-24 17:26:35 +0600

    review add

--- src/Component/Review.js
+++ src/Component/Review.js
@@ -18,13 +18,14 @@ const Review = () => {
       .then((data) => setReview(data));
   }, []);
 
+  const updateReview = review.slice(-6).reverse();
   return (
     <div>
       <section class="mb-32 text-gray-800 text-center">
         <h2 class="text-3xl font-bold mb-12">Our Client Review</h2>
 
         <div class="grid md:grid-cols-3 gap-x-6 lg:gap-x-12">
-          {review?.map((reviewItem) => {
+          {updateReview?.map((reviewItem) => {
             return <ReviewItem reviewItem={reviewItem} />;
           })}
         </div>
